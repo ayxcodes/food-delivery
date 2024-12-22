c35aa08307f10afd37a22509e6f057650dfac9d4
Aylin Celikhan <a.celikhan@outlook.de>
2024-12-22 20:29:03 +0100

    basket functions

--- script/script.js
+++ script/script.js
@@ -1,5 +1,18 @@
-let cartDishes = [];
-let cartPrice = [];
+let dishesInBasket = [];
+let itemCounter = [];
+let itemSubtotal = [];
+let itemsInBasket = [];
+let minimumOrderValue = 20;
+let basketSubTotal;
+let basketTotal = 0;
+
+
+function render() {
+    renderMain();
+    renderSide();
+    renderDrinks();
+    renderBasket()
+}
 
 function renderMain() {
     let allDishes = document.getElementById('main-dishes');
@@ -9,7 +22,7 @@ function renderMain() {
         if (dishes[i].kategorie === 'Burger') {
             for (let j = 0; j < dishes[i].menu.length; j++) {
                 let burger = dishes[i].menu[j];
-                allDishes.innerHTML += getMainDishesTemplate(burger);
+                allDishes.innerHTML += getMainDishesTemplate(burger, i, j);
             }
         }
     }
@@ -23,7 +36,7 @@ function renderSide() {
         if (dishes[i].kategorie === 'Beilagen') {
             for (let j = 0; j < dishes[i].menu.length; j++) {
                 let sides = dishes[i].menu[j];
-                allDishes.innerHTML += getSideDishesTemplate(sides);
+                allDishes.innerHTML += getSideDishesTemplate(sides, i, j);
             }
         }
     }
@@ -37,23 +50,84 @@ function renderDrinks() {
         if (dishes[i].kategorie === 'Getränke') {
             for (let j = 0; j < dishes[i].menu.length; j++) {
                 let drinks = dishes[i].menu[j];
-                allDrinks.innerHTML += getDrinksTemplate(drinks);
+                allDrinks.innerHTML += getDrinksTemplate(drinks, i, j);
             }
         }
     }
 }
 
 
-function addToBasket(i) {
+function renderBasket() {
+    let total = dishesInBasket.reduce((sum, item) => sum + item.total, 0);
     let basket = document.getElementById('basket');
     basket.innerHTML = ``;
+    
+    if (dishesInBasket.length === 0) {
+        basket.innerHTML = '<p class="basket-disclaimer">Dein Warenkorb ist momentan leer.</p>';
+        return;
 
-    basket.innerHTML += getBasketTemplate(i);
+    } else {
+        
+        for (let i = 0; i < dishesInBasket.length; i++) {
+        basket.innerHTML += getBasketTemplate(i);
+    }};
 }
 
 
-function addMeal() {}
+function addToBasket(i, j) {
+    let item = dishes[i].menu[j];
+    let itemName = item.name;
+    let itemPrice = item.price;
+    let itemAmount = 1;
+    let itemTotal = itemPrice * itemAmount;
 
-function subtractMeal() {}
+    let itemAlreadyExistsInBasket = false;
+    let indexOfExistingItemInBasket = -1;
 
-function deleteFromBasket() {}
+    for (let j = 0; j < dishesInBasket.length; j++) {
+        if (dishesInBasket[j].name === itemName) {
+            itemAlreadyExistsInBasket = true;
+            indexOfExistingItemInBasket = j;
+            break;
+        }
+    }
+
+    if (itemAlreadyExistsInBasket) {
+        dishesInBasket[indexOfExistingItemInBasket].amount += itemAmount;
+        dishesInBasket[indexOfExistingItemInBasket].total += itemTotal;
+
+    } else {
+
+        dishesInBasket.push({
+            name: itemName,
+            price: itemPrice,
+            amount: itemAmount,
+            total: itemTotal,
+        });
+    }
+
+    renderBasket();
+}
+
+function subtractMeal(index) {
+    let item = dishesInBasket[index];
+    if (item.amount > 1) {
+        item.amount--;
+        item.total = item.amount * item.price;
+    } else {
+        deleteFromBasket(index);
+    }
+    renderBasket();
+}
+
+function addMeal(index) {
+    let item = dishesInBasket[index];
+    item.amount++;
+    item.total = item.amount * item.price;
+    renderBasket();
+}
+
+function deleteFromBasket(index) {
+    dishesInBasket.splice(index, 1);
+    renderBasket();
+}
--- script/template.js
+++ script/template.js
@@ -1,54 +1,51 @@
-function getMainDishesTemplate(burger) {
+function getMainDishesTemplate(burger, i, j) {
     return `
     <div class="dish">
         <h3>${burger.name}</h3>
         <p>${burger.discription}</p><br>
-        <button onclick="addToBasket()" class="add-btn">+</button>
+        <button onclick="addToBasket(${i}, ${j})" class="add-btn">+</button>
         <span class="price">${burger.price.toFixed(2)}€</span>
     </div>
     `;
 }
 
-function getSideDishesTemplate(sides) {
+function getSideDishesTemplate(sides, i, j) {
     return `
     <div class="dish">
         <h3>${sides.name}</h3>
         <p>${sides.discription}</p><br>
-        <button onclick="addToBasket()" class="add-btn">+</button>
+        <button onclick="addToBasket(${i}, ${j})" class="add-btn">+</button>
         <span class="price">${sides.price.toFixed(2)}€</span>
     </div>
     `;
 }
 
-function getDrinksTemplate(drinks) {
+function getDrinksTemplate(drinks, i, j) {
     return `
     <div class="dish">
         <h3>${drinks.name}</h3>
         <p>${drinks.discription}</p><br>
-        <button onclick="addToBasket()" class="add-btn">+</button>
+        <button onclick="addToBasket(${i}, ${j})" class="add-btn">+</button>
         <span class="price">${drinks.price.toFixed(2)}€</span>
     </div>
     `;
 }
 
 function getBasketTemplate(i) {
+    let item = dishesInBasket[i];
     return `
     <div class="basket">
         <div class="basket-meal-wrapper">
-            <h4>${i.name}</h4>
+            <h4>${item.name}</h4> <!-- Zugriff auf den Namen des Artikels im Warenkorb -->
             <div class="basket-meals">
                 <div class="amount-wrapper">
-                  <button onclick="subtractMeal()" id="subtract-meal" class="basket-button">-</button>
-                  <p>1x</p>
-                  <button onclick="addMeal()" id="add-meal" class="basket-button">+</button>
+                  <button onclick="subtractMeal(${i})" id="subtract-meal" class="basket-button">-</button>
+                  <p>${item.amount}x</p> <!-- Zeige die Menge des Artikels -->
+                  <button onclick="addMeal(${i})" id="add-meal" class="basket-button">+</button>
                 </div>
-                <p class="price">${i.price}</p>
-                <button onclick="deleteFromBasket()" id="delete-meal" class="basket-button">x</button>
+                <p class="price">${item.total.toFixed(2)}€</p> <!-- Zeige den Gesamtpreis des Artikels -->
+                <button onclick="deleteFromBasket(${i})" id="delete-meal" class="basket-button">x</button>
             </div>
-        </div>
-        <div id="basket-total">
-            <h4>Gesamt</h4>
-            <p></p>
         </div>
     </div>
     `;
